cf5725283328cb61a99c454b9b3e226efed034ca
Kaushal Bhingaradia <75636044+kashrtx@users.noreply.github.com>
2024-04-29 13:27:25 -0400

    Website overhaul
    - Improved visuals
    - Contact form works

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ document.querySelectorAll('.project-expand').forEach(button => {
       }
     });
   });
+
+// ------------------------ FORMSPREE CONTACT FORM ------------------------
+const form = document.getElementById("contactForm");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    // Submit the form to Formspree
+    const response = await fetch(form.action, {
+      method: "POST",
+      body: new FormData(form),
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    
+    if (response.ok) {
+      // Display success message
+      msg.innerHTML = "Message Sent Successfully";
+      // Reset the form
+      form.reset();
+      setTimeout(() => { msg.innerHTML = ""; }, 5000);
+    } else {
+      throw new Error("Form submission failed");
+    }
+  } catch (error) {
+    console.error("Error!", error.message);
+  }
+});
